62525f428d2cdd2d873168b82f3d3c6dae1e0c7f
kdraganik <karol.draganik@gmail.com>
2024-12-10 18:14:56 +0100

    Zajęcia 03.12

--- src/app/add/page.js
+++ src/app/add/page.js
@@ -26,14 +26,14 @@ export default function Add() {
     return (
         <div className={styles.container}>
             <h1>Add page</h1>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-                <label htmlFor="link">Link</label>
-                <input id="link" type="text" value={link} onChange={(e) => setLink(e.target.value)}/>
-                <label htmlFor="price">Price</label>
-                <input id="price" type="number" value={price} onChange={(e) => setPrice(e.target.value)}/>
-                <button onClick={handleClick}>Add</button>
+            <form className={styles.formContainer}>
+                <label htmlFor="name" className={styles.formLabel}>Name</label>
+                <input id="name" className={styles.formInput}  type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+                <label htmlFor="link" className={styles.formLabel}>Link</label>
+                <input id="link" className={styles.formInput}  type="text" value={link} onChange={(e) => setLink(e.target.value)}/>
+                <label htmlFor="price" className={styles.formLabel}>Price</label>
+                <input id="price" className={styles.formInput} type="number" value={price} onChange={(e) => setPrice(e.target.value)}/>
+                <button onClick={handleClick} className={styles.formButton}>Add</button>
             </form>
         </div>
     )
